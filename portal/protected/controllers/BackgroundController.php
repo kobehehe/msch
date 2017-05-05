52bffa3227cb1b23e3c29944ebe628c4a6f297af
= <=>
2017-05-05 11:32:00 +0800

    导出pdf文档

--- portal/protected/controllers/BackgroundController.php
+++ portal/protected/controllers/BackgroundController.php
@@ -5531,6 +5531,32 @@ class BackgroundController extends InitController
             ));
     }
 
+    public function actionexport_pdf(){
+        $html = $_POST['html'];
+        ob_start();
+        $html='
+        <link rel="stylesheet" type="text/css" href="css/base3.css" />
+        <link rel="stylesheet" type="text/css" href="css/bill.css" />
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />'.$html;
+        //这儿可以引入生成的Html的样式表  路径可以是绝对路径也可以是相对路径，也可以把样式表文件复制到临时html文件的目录下 
+        $filename = "meisicehua";
+        //save the html page in tmp folder  保存的html临时文件位置 可以是相对路径也是可以是绝对路径 下面用相对路径
+        file_put_contents("{$filename}.html", $html);
+
+        //Clean the output buffer and turn off output buffering
+        ob_end_clean();
+
+        //convert HTML to PDF
+        shell_exec("wkhtmltopdf -q {$filename}.html {$filename}.pdf");
+        if(file_exists("{$filename}.pdf")){
+            header("Content-type:application/pdf");
+            header("Content-Disposition:attachment;filename={$filename}.pdf");
+            echo file_get_contents("{$filename}.pdf");
+        }else{
+            exit;
+        }
+    }
+
     public function actionGet_order_list()
     {
         //参数表
